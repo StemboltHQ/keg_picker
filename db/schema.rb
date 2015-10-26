c6952ef25be0c39aea51b7620db7fbf935a305d7
Daria Sova <daria@freerunningtechnologies.com>
2015-10-26 14:26:22 -0700

    Add username to User
    add User factory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026200919) do
+ActiveRecord::Schema.define(version: 20151026210727) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "brand"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151026200919) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |x|"winniethepooh#{x}@test.com" }
+    sequence(:username) { |x| "Pooh#{x}" }
     password "12345678"
     password_confirmation "12345678"
   end

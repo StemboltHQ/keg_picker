fe1c36c66d2d1495855d137dca8aeeaafe934f92
Daria Sova <daria@freerunningtechnologies.com>
2015-11-02 16:42:04 -0800

    Add method to User for the Google authentication
    add 'Sign-in with Google link'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.find_for_google_oauth2(access_token, _signed_in_resource = nil)
+    data = access_token.info
+    User.where(email: data["email"]).first_or_create! do |user|
+      user.email = data["email"]
+      user.username = data["first_name"]
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'cancan/matchers'
 
 RSpec.describe User, type: :model do
+  AccessToken = Struct.new(:info)
+
   it "is invalid without parameters" do
     expect(described_class.new().valid?).to be false
   end
@@ -14,6 +16,35 @@ RSpec.describe User, type: :model do
 
       it "has an ability to edit" do
         expect(ability).to be_able_to(:edit, Beer.new)
+      end
+    end
+  end
+
+  describe '#find_for_google_oauth2' do
+    let(:username) { "RobertK" }
+    subject { User.find_for_google_oauth2(AccessToken.new("email" => email)) }
+
+    context "when user exists" do
+      let!(:user) { FactoryGirl.create(:user, username: username) }
+      let(:email) { user.email }
+
+      it { is_expected.to eq(user) }
+
+      it "should not create the second user" do
+        expect(User.count).to eq 1
+      end
+    end
+
+    context "when user does not exist yet" do
+      let(:email) { "newuser@gmail.com" }
+
+      it "creates a new user" do
+        expect { subject }.to change { User.count }.by(1)
+      end
+
+      it "creates a user with the correct attributes" do
+        subject.update_attributes("username" => username)
+        expect(User.last).to have_attributes( email: "newuser@gmail.com", username: "RobertK" )
       end
     end
   end

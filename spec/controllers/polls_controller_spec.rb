130182a36912ec7c94631737e3a8fe6564efcbc2
Daria Sova <daria@freerunningtechnologies.com>
2015-11-19 10:55:01 -0800

    Add page with current polls
    add delete action

--- spec/controllers/polls_controller_spec.rb
+++ spec/controllers/polls_controller_spec.rb
@@ -1,11 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe PollsController, type: :controller do
+  describe "GET #index" do
+    subject { get :index }
+
+    let(:polls) { FactoryGirl.create_list :poll, 2 }
+    specify { expect(subject.status).to eq 200 }
+    it { is_expected.to render_template :index }
+
+    it "assigns all polls as @polls" do
+      subject
+      expect(assigns(:polls)).to eq polls
+    end
+  end
+
   describe "POST #create" do
     subject { post :create }
 
     it "creates a new poll" do
       expect { subject }.to change { Poll.count }.by(1)
     end
+  end
+
+  describe "DELETE #destroy" do
+    subject { delete :destroy, id: poll.to_param }
+
+    let!(:poll) { FactoryGirl.create :poll }
+
+    it "deletes a beer" do
+      expect { subject }.to change { Poll.count }.by(-1)
+    end
+
+    it { is_expected.to redirect_to "/polls" }
   end
 end

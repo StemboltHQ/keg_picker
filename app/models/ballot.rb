28dbcab5ae808d58b46df955405ccdc45280155f
Daria Sova <daria@freerunningtechnologies.com>
2015-11-20 11:55:30 -0800

    Allow users to make only one vote per poll

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -2,4 +2,5 @@ class Ballot < ActiveRecord::Base
   belongs_to :user
   belongs_to :beer
   belongs_to :poll
+  validates :user_id, uniqueness: { scope: :poll, message: "You can vote only once" }
 end

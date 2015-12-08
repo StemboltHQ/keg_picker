faa7eef7debfd83768f877360f1083985d2147d6
Daria Sova <daria@freerunningtechnologies.com>
2015-12-08 15:55:59 -0800

    Add next and previous methods to the Poll class
    allows going through polls history

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -3,4 +3,19 @@ require 'rails_helper'
 RSpec.describe Poll, type: :model do
   it { is_expected.to have_many :ballots }
   it { is_expected.to have_many :users }
+
+  let!(:polls) { FactoryGirl.create_list :poll, 2 }
+  describe '#previous_poll' do
+    subject { Poll.last.previous }
+    it "should return the previous poll" do
+      expect(subject).to eq(Poll.first)
+    end
+  end
+
+  describe '#next_poll' do
+    subject { Poll.first.next }
+    it "should return the next poll" do
+      expect(subject).to eq(Poll.last)
+    end
+  end
 end

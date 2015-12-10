1ae0caf47e4ac76e4d844e5b1ac2751bde990b6b
Daria Sova <daria@freerunningtechnologies.com>
2015-12-09 16:02:01 -0800

    Allow switching to the previous charts
    put js loguc into a separate file

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,8 +1,10 @@
 $(function() {
   $('.row a').on('click',  function(event)  {
     event.preventDefault();
-    $.get('api/polls/2', function(data){
-      new Chartkick.PieChart($('.chart')[0], data);
+    var previous_poll_id = $('#previous-poll').data('poll_id');
+    $.get("api/polls/" + previous_poll_id, function(data){
+      new Chartkick.PieChart($('.chart')[0], data.chart_data);
+      $('#previous-poll').data('poll_id', data.prev_id);
     });
   });
 });

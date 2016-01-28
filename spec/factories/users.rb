d1e8bcea9f5b639c8bde6767156c084ea3c64087
Gray Gilmore <graygilmore@gmail.com>
2016-01-27 23:19:02 -0800

    Use example.com for fake emails

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |x|"winniethepooh#{x}@test.com" }
+    sequence(:email) { |x|"winniethepooh#{x}@example.com" }
     sequence(:username) { |x| "Pooh#{x}" }
     password "12345678"
     password_confirmation "12345678"
 
     factory :admin do
-      sequence(:email) { |x| "admin#{x}@test.com" }
+      email "admin@example.com"
+
       after(:create) do |user|
         user.add_role :admin
       end

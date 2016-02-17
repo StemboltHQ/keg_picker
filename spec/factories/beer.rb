01418bea956e3fe7343b62f94932093ba54c4d11
Gray Gilmore <graygilmore@gmail.com>
2016-02-16 16:32:16 -0800

    Make sure the brewery value is displayed

--- spec/factories/beer.rb
+++ spec/factories/beer.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :beer do
-    sequence(:name) { |n| "Beer ##{n} #{["Pilsner", "Lager", "Pale Ale", "IPA", "Stout"].sample}" }
+    brewery { ["Phillips", "Hoyne", "Driftwood"].sample }
+    sequence(:name) { |n| "#{["Pilsner", "Lager", "Pale Ale", "IPA", "Stout"].sample} ##{n}" }
   end
 end

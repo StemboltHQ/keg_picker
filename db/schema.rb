c2089cc5c1c6c9f0d5fc40edf7e61f543f668f1b
Gray Gilmore <graygilmore@gmail.com>
2016-02-16 16:35:30 -0800

    Add breweries to polls
    There are cases where we want polls to be specific to breweries to make
    ordering easier. This will allow you to set a brewery on a poll.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128062342) do
+ActiveRecord::Schema.define(version: 20160217003421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160128062342) do
     t.datetime "updated_at",                 null: false
     t.datetime "ended_at"
     t.integer  "winner_id"
+    t.string   "brewery"
   end
 
   add_index "polls", ["winner_id"], name: "index_polls_on_winner_id", using: :btree

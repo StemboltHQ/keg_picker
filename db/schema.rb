727720827d8bb97a9f85c5eb697afd3853b99b34
Gray Gilmore <graygilmore@gmail.com>
2016-03-24 16:42:38 -0700

    Add a style column to beers
    We want to be able to easily let people know what kind of beer this is.
    This can later be migrated into its own model if we have other use cases
    for it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217003421) do
+ActiveRecord::Schema.define(version: 20160324233207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160217003421) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "brewery"
+    t.string   "style"
   end
 
   create_table "polls", force: :cascade do |t|

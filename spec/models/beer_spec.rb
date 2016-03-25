8d98a38a8aca769922a416eeda778fa3767592a1
Gray Gilmore <graygilmore@gmail.com>
2016-03-24 22:25:22 -0700

    Add a method to format beer names
    We having beer names all over the app and it would be great to create a
    consistent naming scheme. This commit addresses that!

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Beer, type: :model do
+  let(:beer) do
+    FactoryGirl.create(
+      :beer,
+      brewery: "Hoyne",
+      name: "Devil's Dream",
+      style: "IPA"
+    )
+  end
 
   it { is_expected.to have_many :ballots }
 
@@ -14,5 +22,13 @@ RSpec.describe Beer, type: :model do
 
   it "is invalid with a name less than three characters long" do
     expect(described_class.new(name: "No").valid?).to be false
+  end
+
+  describe "#display_name" do
+    subject { beer.display_name }
+
+    it "nicely formats the beer for display to the front-end" do
+      expect(subject).to eq "Hoyne Devil's Dream IPA"
+    end
   end
 end
